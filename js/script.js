f75b4b9e2cf0532e852995cdccd134303d76ac11
chepceline <chepceline25@gmail.com>
2022-03-13 02:55:24 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#development-image").click(function(){
+        $("#development-image").slideDown('500').hide('1000');
+        $("#development-image").show('1500');
+    });
+    $("#development-image").click(function(){
+        $("#development-image").slideUp('1500');
+        $("#development-image").slideDown('1500');
+    });
+}) 
