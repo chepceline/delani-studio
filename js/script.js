bae3bd699f8618685a909a4fe6838219de77e9b3
chepceline <chepceline25@gmail.com>
2022-03-13 03:13:41 +0300

    initial

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $("#development-image").click(function(){
         $("#development-image").slideDown('500').hide('1000');
-        $("#development-image").show('1500');
+        $("#development").show('1500');
     });
-    $("#development-image").click(function(){
-        $("#development-image").slideUp('1500');
+    $("#development").click(function(){
+        $("#development").slideUp('1500');
         $("#development-image").slideDown('1500');
     });
 }) 
@@ -13,10 +13,10 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#design-image").click(function(){
         $("#design-image").slideDown('500').hide('1000');
-        $("#design-image").show('1500');
+        $("#design").show('1500');
     });
-    $("#design-image").click(function(){
-        $("#design-image").slideUp('1500');
+    $("#design").click(function(){
+        $("#design").slideUp('1500');
         $("#design-image").slideDown('1500');
     });
 }) 
@@ -25,10 +25,10 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#product-image").click(function(){
         $("#product-image").slideDown('500').hide('1000');
-        $("#product-image").show('1500');
+        $("#product").show('1500');
     });
-    $("#product-image").click(function(){
-        $("#product-image").slideUp('1500');
+    $("#product").click(function(){
+        $("#product").slideUp('1500');
         $("#product-image").slideDown('1500');
     });
 }) 
