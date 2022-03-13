929aeb2506994e3472560dadc43f6dc8b2c513ad
chepceline <chepceline25@gmail.com>
2022-03-13 03:07:59 +0300

    initial

--- js/script.js
+++ js/script.js
@@ -8,3 +8,27 @@ $(document).ready(function(){
         $("#development-image").slideDown('1500');
     });
 }) 
+
+
+$(document).ready(function(){
+    $("#design-image").click(function(){
+        $("#design-image").slideDown('500').hide('1000');
+        $("#design-image").show('1500');
+    });
+    $("#design-image").click(function(){
+        $("#design-image").slideUp('1500');
+        $("#design-image").slideDown('1500');
+    });
+}) 
+
+
+$(document).ready(function(){
+    $("#product-image").click(function(){
+        $("#product-image").slideDown('500').hide('1000');
+        $("#product-image").show('1500');
+    });
+    $("#product-image").click(function(){
+        $("#product-image").slideUp('1500');
+        $("#product-image").slideDown('1500');
+    });
+}) 
