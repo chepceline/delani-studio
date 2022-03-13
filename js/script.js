85a431fd2d4ddbbbd857ebd0a9ddd2b3319a080c
chepceline <chepceline25@gmail.com>
2022-03-13 12:13:52 +0300

    initial

--- js/script.js
+++ js/script.js
@@ -35,69 +35,56 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $("#work1").mouseover(function(){
-        $("#overlay").show();
+      $("#overlay").show();
     }).mouseout(function(){
-        $("#overlay").hide();
+      $("#overlay").hide();
     });
-})
-
-$(document).ready(function(){
+  });
+  $(document).ready(function(){
     $("#work2").mouseover(function(){
-        $("#overlay2").show();
+      $("#overlay2").show();
     }).mouseout(function(){
-        $("#overlay2").hide();
+      $("#overlay2").hide();
     });
-})
-
-$(document).ready(function(){
+  });
+  $(document).ready(function(){
     $("#work3").mouseover(function(){
-        $("#overlay3").show();
+      $("#overlay3").show();
     }).mouseout(function(){
-        $("#overlay3").hide();
+      $("#overlay3").hide();
     });
-})
-
-
-$(document).ready(function(){
+  });
+  $(document).ready(function(){
     $("#work4").mouseover(function(){
-        $("#overlay4").show();
+      $("#overlay4").show();
     }).mouseout(function(){
-        $("#overlay4").hide();
+      $("#overlay4").hide();
     });
-})
-
-$(document).ready(function(){
+  });
+  
+  $(document).ready(function(){
     $("#work5").mouseover(function(){
-        $("#overlay5").show();
+      $("#overlay5").show();
     }).mouseout(function(){
-        $("#overlay5").hide();
+      $("#overlay5").hide();
     });
-})
-
-$(document).ready(function(){
     $("#work6").mouseover(function(){
-        $("#overlay6").show();
+      $("#overlay6").show();
     }).mouseout(function(){
-        $("#overlay6").hide();
+      $("#overlay6").hide();
     });
-})
-
-$(document).ready(function(){
     $("#work7").mouseover(function(){
-        $("#overlay7").show();
+      $("#overlay7").show();
     }).mouseout(function(){
-        $("#overlay7").hide();
+      $("#overlay7").hide();
     });
-})
-
-$(document).ready(function(){
     $("#work8").mouseover(function(){
-        $("#overlay8").show();
+      $("#overlay8").show();
     }).mouseout(function(){
-        $("#overlay8").hide();
+      $("#overlay8").hide();
     });
-})
-
+  });
+  
 $(document).ready(function(){
     $("form#form34A").submit(function(event){
     
